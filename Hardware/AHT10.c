159d7c7c0819868bca00cd2ea81dfd3bcece6a02
YuXiang <156730751@qq.com>
2026-04-13 15:25:24 +0800

    new driver for AHT10

--- Hardware/AHT10.c
+++ Hardware/AHT10.c
@@ -1,55 +1,97 @@
 #include "stm32f10x.h"
-#include "SoftI2C.h"
+//#include "SoftI2C.h"
 #include "Delay.h"
+
+void AHT10_WaitEvent(I2C_TypeDef* I2Cx, uint32_t I2C_EVENT)
+{
+    uint16_t Timeout;
+    Timeout = 10000;
+    while(I2C_CheckEvent(I2Cx,I2C_EVENT) != SUCCESS)
+    {
+        Timeout--;
+        if(Timeout == 0)
+        {
+            
+            break;
+        }
+    }
+}
+
+
 
 void AHT10_Init(void)
 {
-    SoftI2C_Init();
-    
-    SoftI2C_Start();
-    SoftI2C_SendByte(0x70 | 0x00);
-    SoftI2C_ReceiveACK();
-    SoftI2C_SendByte(0xE0);
-    SoftI2C_ReceiveACK();
-    SoftI2C_Stop();
     Delay_ms(20);
+    I2C_GenerateSTART(I2C1,ENABLE);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
+    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Transmitter);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED);
+    I2C_SendData(I2C1,0xE0);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED);
+    I2C_GenerateSTOP(I2C1,ENABLE);
 }
 
 void AHT10_StartMeas()
 {
-    SoftI2C_Start();
-    SoftI2C_SendByte(0x70 | 0x00);
-    SoftI2C_ReceiveACK();
-    SoftI2C_SendByte(0xac);
-    SoftI2C_ReceiveACK();
-    SoftI2C_SendByte(0x33);
-    SoftI2C_ReceiveACK();
-    SoftI2C_SendByte(0x00);
-    SoftI2C_ReceiveACK();
-    SoftI2C_Stop();
+//    SoftI2C_Start();
+//    SoftI2C_SendByte(0x70 | 0x00);
+//    SoftI2C_ReceiveACK();
+//    SoftI2C_SendByte(0xac);
+//    SoftI2C_ReceiveACK();
+//    SoftI2C_SendByte(0x33);
+//    SoftI2C_ReceiveACK();
+//    SoftI2C_SendByte(0x00);
+//    SoftI2C_ReceiveACK();
+//    SoftI2C_Stop();
+    I2C_GenerateSTART(I2C1,ENABLE);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
+    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Transmitter);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED);
+    I2C_SendData(I2C1,0xac);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTING);
+    I2C_SendData(I2C1,0x33);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTING);
+    I2C_SendData(I2C1,0x00);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED);
+    I2C_GenerateSTOP(I2C1,ENABLE);
     Delay_ms(80);
 }
 
 uint8_t AHT10_ReadData(float *AHT10_Temp,float *AHT10_RH)
 {
-    uint8_t Data[5],i;
-    SoftI2C_Start();
-    SoftI2C_SendByte(0x70 | 0x01);
-    SoftI2C_ReceiveACK();
-    if((SoftI2C_ReceiveByte() | 0x80) == 1)
+    uint8_t Data[6],i;
+//    SoftI2C_Start();
+//    SoftI2C_SendByte(0x70 | 0x01);
+//    SoftI2C_ReceiveACK();
+//    for(i = 0;i < 6;i++)
+//    {
+//    SoftI2C_SendACK(0);
+//    Data[i] = SoftI2C_ReceiveByte();
+//    }
+//    SoftI2C_SendACK(1);
+//    SoftI2C_Stop();
+    I2C_GenerateSTART(I2C1,ENABLE);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
+    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Receiver);
+    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED);
+    for(i = 0;i < 6;i++)
+    {
+        if(i == (6 - 1))
+        {
+            I2C_AcknowledgeConfig(I2C1,DISABLE);
+            I2C_GenerateSTOP(I2C1,ENABLE);
+        }
+        Data[i] = I2C_ReceiveData(I2C1);
+        AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_RECEIVED);
+    }
+    
+    if((Data[0] & 0x80) == 1)
     {
         return 1;
     }
-    for(i = 0;i < 5;i++)
-    {
-    SoftI2C_SendACK(0);
-    Data[i] = SoftI2C_ReceiveByte();
-    }
-    SoftI2C_SendACK(1);
-    SoftI2C_Stop();
-    *AHT10_RH = ((((uint32_t)Data[0] << 12 )| ((uint32_t)Data[1] << 8) |
-                        ((uint32_t)Data[2] >> 8)) / 1048576.0) * 100.0;
-    *AHT10_Temp = ((((uint32_t)(Data[2] & 0x0F) << 16 )| ((uint32_t)Data[3] << 8) |
-                    (uint32_t)Data[4] )/ 1048576.0) * 200.0 - 50.0;
-    return Data[4];
+    *AHT10_RH = ((((uint32_t)Data[1] << 12 )| ((uint32_t)Data[2] << 8) |
+                        ((uint32_t)Data[3] >> 8)) / 1048576.0) * 100.0;
+    *AHT10_Temp = ((((uint32_t)(Data[3] & 0x0F) << 16 )| ((uint32_t)Data[4] << 8) |
+                    (uint32_t)Data[5] )/ 1048576.0) * 200.0 - 50.0;
+    return 0;
 }
