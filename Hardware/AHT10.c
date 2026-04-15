ffae01bc119f25c7377c6f8e5fc64a8c3ff19244
YuXiang <156730751@qq.com>
2026-04-15 14:58:41 +0800

    finally version for the AHT10 driver

--- Hardware/AHT10.c
+++ Hardware/AHT10.c
@@ -1,6 +1,5 @@
 #include "stm32f10x.h"
-//#include "SoftI2C.h"
-#include "HardwareI2CInit.h"
+#include "SoftI2C.h"
 #include "Delay.h"
 
 void AHT10_WaitEvent(I2C_TypeDef* I2Cx, uint32_t I2C_EVENT)
@@ -21,111 +20,67 @@ void AHT10_WaitEvent(I2C_TypeDef* I2Cx, uint32_t I2C_EVENT)
 void AHT10_Init(void)
 {
     uint8_t aht10_status;
-//    SoftI2C_Init();
-//    SoftI2C_Start();
-//    SoftI2C_SendByte(0x70 | 0x01);
-//    SoftI2C_ReceiveACK();
-//    if(SoftI2C_ReceiveByte() & 0x08)
-//    {
-//        SoftI2C_SendACK(1);
-//    }
-//    else
-//    {
-//        SoftI2C_SendACK(1);
-//        SoftI2C_Stop();
-//        SoftI2C_Start();
-//        SoftI2C_SendByte(0x70 | 0x00);
-//        SoftI2C_ReceiveACK();
-//        SoftI2C_SendByte(0xE0);
-//        SoftI2C_ReceiveACK();
-//    }
-//    SoftI2C_Stop();
-    I2C_GenerateSTART(I2C1,ENABLE);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
-    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Receiver);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED);
-    I2C_AcknowledgeConfig(I2C1,DISABLE);
-    I2C_GenerateSTOP(I2C1,ENABLE);
-    aht10_status = I2C_ReceiveData(I2C1);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_RECEIVED);
-    if((aht10_status & 0x08) == 0)
+    SoftI2C_Init();
+    SoftI2C_Start();
+    SoftI2C_SendByte(0x70 | 0x01);
+    SoftI2C_ReceiveACK();
+    if(SoftI2C_ReceiveByte() & 0x08)
     {
-        I2C_GenerateSTART(I2C1,ENABLE);
-        AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
-        I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Transmitter);
-        AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED);
-        I2C_SendData(I2C1,0xE0);
-        AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED);
-        I2C_GenerateSTOP(I2C1,ENABLE);
+        SoftI2C_SendACK(1);
     }
-    
+    else
+    {
+        SoftI2C_SendACK(1);
+        SoftI2C_Stop();
+        SoftI2C_Start();
+        SoftI2C_SendByte(0x70 | 0x00);
+        SoftI2C_ReceiveACK();
+        SoftI2C_SendByte(0xE1);
+        SoftI2C_ReceiveACK();
+    }
+    SoftI2C_Stop();
     Delay_ms(100);
     
 }
 
 void AHT10_StartMeas()
 {
-//    SoftI2C_Start();
-//    SoftI2C_SendByte(0x70 | 0x00);
-//    SoftI2C_ReceiveACK();
-//    SoftI2C_SendByte(0xac);
-//    SoftI2C_ReceiveACK();
-//    SoftI2C_SendByte(0x33);
-//    SoftI2C_ReceiveACK();
-//    SoftI2C_SendByte(0x00);
-//    SoftI2C_ReceiveACK();
-//    SoftI2C_Stop();
-    I2C_GenerateSTART(I2C1,ENABLE);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
-    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Transmitter);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED);
-    I2C_SendData(I2C1,0xac);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTING);
-    I2C_SendData(I2C1,0x33);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTING);
-    I2C_SendData(I2C1,0x00);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED);
-    I2C_GenerateSTOP(I2C1,ENABLE);
+    SoftI2C_Start();
+    SoftI2C_SendByte(0x70 | 0x00);
+    SoftI2C_ReceiveACK();
+    SoftI2C_SendByte(0xac);
+    SoftI2C_ReceiveACK();
+    SoftI2C_SendByte(0x33);
+    SoftI2C_ReceiveACK();
+    SoftI2C_SendByte(0x00);
+    SoftI2C_ReceiveACK();
+    SoftI2C_Stop();
     Delay_ms(80);
 }
 
 uint8_t AHT10_ReadData(float *AHT10_Temp,float *AHT10_RH)
 {
     uint8_t Data[6],i;
-//    SoftI2C_Start();
-//    SoftI2C_SendByte(0x70 | 0x01);
-//    SoftI2C_ReceiveACK();
-//    for(i = 0;i < 6;i++)
-//    {
-
-//        Data[i] = SoftI2C_ReceiveByte();
-//        if(i == 5)
-//        {
-//            SoftI2C_SendACK(1);
-//        }
-//        else
-//        {
-//            SoftI2C_SendACK(0);
-//        }
-//    }
-//    SoftI2C_Stop();
-    I2C_GenerateSTART(I2C1,ENABLE);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT);
-    I2C_Send7bitAddress(I2C1,0x38,I2C_Direction_Receiver);
-    AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED);
+    SoftI2C_Start();
+    SoftI2C_SendByte(0x70 | 0x01);
+    SoftI2C_ReceiveACK();
     for(i = 0;i < 6;i++)
     {
-        if(i == (6 - 1))
+
+        Data[i] = SoftI2C_ReceiveByte();
+        if(i == 5)
         {
-            I2C_AcknowledgeConfig(I2C1,DISABLE);
-            I2C_GenerateSTOP(I2C1,ENABLE);
+            SoftI2C_SendACK(1);
         }
-        AHT10_WaitEvent(I2C1,I2C_EVENT_MASTER_BYTE_RECEIVED);
-        Data[i] = I2C_ReceiveData(I2C1);
+        else
+        {
+            SoftI2C_SendACK(0);
+        }
     }
+    SoftI2C_Stop();
     I2C_AcknowledgeConfig(I2C1,ENABLE);
     *AHT10_RH = ((uint32_t)Data[1] << 12 )| ((uint32_t)Data[2] << 8) |
-                        ((uint32_t)Data[3] >> 8);
+                        ((uint32_t)Data[3] >> 4);
     *AHT10_RH = *AHT10_RH / 1048576.0 * 100.0;
     *AHT10_Temp = ((uint32_t)(Data[3] & 0x0F) << 16 )| ((uint32_t)Data[4] << 8) |
                     (uint32_t)Data[5];
@@ -134,3 +89,4 @@ uint8_t AHT10_ReadData(float *AHT10_Temp,float *AHT10_RH)
     return !!(Data[0] & 0x80);
 
 }
+
